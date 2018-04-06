8def3b133d668d75329488d8273d412bcb9b435e
EduardoCL <35414231+EduardoCortesL@users.noreply.github.com>
2018-04-06 11:42:29 -0400

    Update BlocksToken.sol

--- TokenERC20/TokenSeparateFiles/Contracts/BlocksToken.sol
+++ TokenERC20/TokenSeparateFiles/Contracts/BlocksToken.sol
@@ -9,8 +9,8 @@ contract BlocksToken is StandardToken{
 
     string public name = "Blocks";
     string public symbol = "BLOX";
-    uint public decimals = 6;
-    uint public INITIAL_SUPPLY = 100000000000000; // initia supply is 100,000,000
+    uint public decimals = 15;
+    uint public INITIAL_SUPPLY = 100000000000000000000000; // initia supply is 100,000,000
 
     function BlocksToken() {
         totalSupply = INITIAL_SUPPLY;
